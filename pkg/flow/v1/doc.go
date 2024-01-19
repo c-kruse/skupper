39360a65bc79eab3312c4835c4c445b1ab6bbb5e
c-kruse <ctkruse99@gmail.com>
2024-01-19 13:04:39 -0800

    Add pkg/flow/v1 defining vanflow message and record types
    Signed-off-by: c-kruse <ctkruse99@gmail.com>

---
+++ pkg/flow/v1/doc.go
@@ -0,0 +1,8 @@
+/*
+Package v1 defines types for the message and record types specified in the
+VanFlow specification.
+
+Record types are partially generated based off the specification in
+v1/spec.yaml. Run `go generate` to rebuild record type definitions
+*/
+package v1
